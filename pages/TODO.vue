ac9e394b04400e71f4dc9774ab366899fec07e3f
torerik123 <62653782+torerik123@users.noreply.github.com>
2023-08-13 14:01:12 +0200

    Update packages

--- pages/TODO.vue
+++ pages/TODO.vue
@@ -1,11 +1,11 @@
 <template>
 	<v-list>
 		<v-list-item>Font type</v-list-item>
+		<v-list-item>Enable vuetify treeshaking</v-list-item>
 		<v-list-item>Custom 404</v-list-item>
 		<v-list-item>Image sizes => DatoCMS not setting srcSet? </v-list-item>
 		<v-list-item>Image carousel => set same height so next/prev btn always in same place</v-list-item>
 		<v-list-item>Get favicon from DatoCMS</v-list-item>
-		<v-list-item>Enable vuetify treeshaking</v-list-item>
 		<v-list-item>Images alt text</v-list-item>
 		<v-list-item>Nuxt image</v-list-item>
 		<v-list-item>Placeholder images/color block</v-list-item>
